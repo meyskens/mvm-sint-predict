91475c5d19e88a2cfd7b1b2656af4f6d4a8e9859
Maartje Eyskens <maartje@eyskens.me>
2019-10-16 14:51:55 +0200

    use pwd instead of gopath
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- script/generate-proto.sh
+++ script/generate-proto.sh
@@ -3,4 +3,4 @@ if [[ "$GOPATH" == "" ]]; then
     export GOPATH="$HOME/go"
 fi
 export PATH=$PATH:$GOPATH/bin # fix for GOPATH not being in PATH
-protoc -I $GOPATH/src --go_out=plugins=grpc:$GOPATH/src $GOPATH/src/github.com/meyskens/mvm-sint-predict/pb/sint.proto
+protoc -I "$(pwd)" --go_out=plugins=grpc:"$(pwd)" "$(pwd)"/pb/sint.proto
